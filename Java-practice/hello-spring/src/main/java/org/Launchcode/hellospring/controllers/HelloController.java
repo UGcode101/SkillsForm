41085bcf9e43aee6be121a5fec1b03eaf9f19bbe
UGcode101 <142420124+UGcode101@users.noreply.github.com>
2024-05-01 21:23:28 -0500

    Conditional Exercise

--- Java-practice/hello-spring/src/main/java/org/Launchcode/hellospring/controllers/HelloController.java
+++ Java-practice/hello-spring/src/main/java/org/Launchcode/hellospring/controllers/HelloController.java
@@ -1,47 +1,55 @@
 package org.Launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@ResponseBody
 @RequestMapping("hello")
 public class HelloController {
 
-//    @GetMapping ("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!" ;
-//    }
-    //lives /hello/goodbye
-    @GetMapping ("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!" ;
-    }
-    //lives /hello/hello
-    //Handles request of the form/hello?name=LaunchCode
-   @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello," + name + "!" ;
+    // Default handler for "/hello"
+    @GetMapping("")
+    @ResponseBody
+    public String helloDefault() {
+        return "Welcome to the Hello page! Please use /form to access the form.";
     }
 
-    //Handles requests of the form /hello/LaunchCode
-    @GetMapping("Hello/{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!" ;
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye, Spring!";
+    }
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";  // Assumes there is a "hello.html" in src/main/resources/templates
+    }
+
+    // Updated to use lower-case URL for consistency
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model){
+        String greeting =  "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("form")
-    public String helloform(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" +
-                "<input type='text' name= 'name'>" +
-                "<input type='submit' value= 'Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
-
+    public String helloForm(){
+        return "form";  // Returns the name of the view to be rendered, assumes "form.html" exists
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
